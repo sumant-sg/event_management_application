21f6de22957c388e02158194ee29dbb4f5144709
sumant <skbehera67@gmail.com>
2026-01-31 02:54:34 +0530

    Second commit

--- src/main/java/com/example/event_management/security/EventManagementUsernamePwdAuthenticationProvider.java
+++ src/main/java/com/example/event_management/security/EventManagementUsernamePwdAuthenticationProvider.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class EazySchoolUsernamePwdAuthenticationProvider
+public class EventManagementUsernamePwdAuthenticationProvider
         implements AuthenticationProvider
 {
     @Autowired
